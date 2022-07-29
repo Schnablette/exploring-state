f95150e6941274403db1988d87d0437cc8994fd5
Ann Schnabel <anntschnabel@gmail.com>
2022-07-29 17:32:59 -0400

    king contract

---
+++ contracts/DonationKing.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+contract DonationKing {
+    address payable public king;
+    uint256 internal totalAmountAvailable;
+
+    function setKing(address payable _king) public {
+        king = _king;
+    }
+
+    function donate() public {}
+
+    function distribute() public {
+        king.transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+}
--- contracts/StateTypes.sol
+++ contracts/StateTypes.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.9;
 
 contract StateTypes {
-    bytes16 private immutable num;
-    bool banana;
-    string public greeting = "Hello, Friend";
-    uint64 constant num2 = 34576;
+    // bytes16 private immutable num;
+    // bool banana;
+    // string public greeting = "Hello, Friend";
+    // uint64 constant num2 = 34576;
 }
