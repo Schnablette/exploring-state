e14451f4f637514d5171026480ca9488b16c24e4
Ann Schnabel <anntschnabel@gmail.com>
2022-07-28 22:35:35 -0400

    Wrote different states

---
+++ contracts/StateTypes.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract StateTypes {
+    bytes16 private immutable num;
+    bool banana;
+    string public greeting = "Hello, Friend";
+    uint64 constant num2 = 34576;
+}
